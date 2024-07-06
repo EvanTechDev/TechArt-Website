c56d766071c735fe14cf8cef3ceb8fe831346878
Dev.Huang <18778418865@163.com>
2024-07-06 21:28:11 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,8 +10,8 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: 'Protecting privacy from the root.', // 英雄区文字
-  STARTER_HERO_TITLE_2: '', // 英雄区文字
+  STARTER_HERO_TITLE_1: 'Tech-Art', // 英雄区文字
+  STARTER_HERO_TITLE_2: 'Protecting privacy from the root.', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: 'About us', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
