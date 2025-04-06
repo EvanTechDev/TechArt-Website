179a0922f946189d4ce0a6c40057be2b36e5964c
Evan Huang <evan.huang000@proton.me>
2025-04-06 18:23:08 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -211,7 +211,7 @@ const CONFIG = {
       STARTER_TEAM_ITEM_DESCRIPTION: 'Founder & Developer'
     },
     {
-      STARTER_TEAM_ITEM_AVATAR: 'https://cdn.xyehr.cn/images/Image_1712299099839.jpg',
+      STARTER_TEAM_ITEM_AVATAR: 'https://cdn.xyehr.cn/images/Evan.png',
       STARTER_TEAM_ITEM_NICKNAME: 'Evan Huang',
       STARTER_TEAM_ITEM_DESCRIPTION: 'Founder & Marketing & Developer'
     },
